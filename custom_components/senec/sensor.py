5ad4a5dab5f9af524fb9e9b2c467d5be2b98dcfd
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-11-22 22:36:19 +0100

    Extend sensor to store value and catch wrong api

--- custom_components/senec/sensor.py
+++ custom_components/senec/sensor.py
@@ -4,6 +4,7 @@ import logging
 from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import HomeAssistantType
+from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.util import slugify
 from homeassistant.const import CONF_TYPE
 
@@ -59,7 +60,7 @@ async def async_setup_entry(hass: HomeAssistantType, config_entry: ConfigEntry, 
 
     async_add_entities(entities)
 
-class SenecSensor(SenecEntity, SensorEntity):
+class SenecSensor(SenecEntity, SensorEntity, RestoreEntity):
     """Sensor for the single values (e.g. pv power, ac power)."""
 
     def __init__(
@@ -83,6 +84,7 @@ class SenecSensor(SenecEntity, SensorEntity):
         # to set the '_attr_has_entity_name' to trigger the calls to the localization framework!
         self._attr_translation_key = key
         self._attr_has_entity_name = True
+        self._state: float | None = None
 
     @property
     def state(self):
@@ -90,11 +92,30 @@ class SenecSensor(SenecEntity, SensorEntity):
         sensor = self.entity_description.key
         value = getattr(self.coordinator.senec, sensor)
         #_LOGGER.debug( str(sensor)+' '+ str(type(value)) +' '+str(value))
-        if type(value) != type(False):
-            try:
-                rounded_value = round(float(value), 2)
-                return rounded_value
-            except (ValueError, TypeError):
-                return value
-        else:
+        if isinstance(value, bool):
             return value
+        try:
+            value = round(float(value), 2)
+        except (ValueError, TypeError):
+            return value
+            
+        # do not update if value is lower than current state
+        # this is only an issue for _total sensors
+        # since the API may return false values in this case
+        needs_attention = "_total" in str(sensor)
+        if not needs_attention:
+            return value
+        if ((self._state is not None) and (value < self._state)):
+            return self._state
+        self._state = value
+        return self._state
+
+    async def async_added_to_hass(self) -> None:
+        """Call when entity about to be added to Home Assistant."""
+        await super().async_added_to_hass()
+        # get the last known state
+        state = await self.async_get_last_state()
+        # most of the values, are float,
+        # we could also check here for only _total entities
+        if state is not None and isinstance(state, float):
+            self._state = float(state.state)
